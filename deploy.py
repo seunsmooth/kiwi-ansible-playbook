a1f953d9bd1a2347eea413d176de1f0284254441
yren <yufei.ren@thomsonreuters.com>
2016-11-08 23:57:59 +0800

    add playbook

--- deploy.py
+++ deploy.py
@@ -8,3 +8,17 @@ import boto3
 import urllib2
 from subprocess import call
 
+# coding: utf-8
+
+__Author__ = 'yren'
+__Date__ = 'Nov 8, 2016'
+
+USAGE = '''
+Usage:
+    python {script_name} {env} {container} {tag}
+
+Example:
+    python deploy.py qa container-name tag
+'''
+
+PLAYBOOK_DIR = "./playbook/"
