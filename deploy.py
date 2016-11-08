f31fdfbd23abb2e23594f4122ffd7bc688244460
yren <yufei.ren@thomsonreuters.com>
2016-11-08 23:52:55 +0800

    add deploy.py

---
+++ deploy.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import os
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+import argparse
+import subprocess
+import json
+import boto3
+import urllib2
+from subprocess import call
+
