9fe2cd03071061c133d70b045f89337b50450ea7
Yufei Ren <yufei.ren@thomsonreuters.com>
2017-06-14 17:59:20 +0800

    minor change

--- utils/aws_jump.sh
+++ utils/aws_jump.sh
@@ -3,11 +3,13 @@
 ### Date: 2017.06.14
 ### Desc: This shell use for aws jump util
 
-## echo colorful text
-red="\e[31m"
-purple="\e[35m"
-normal="\e[m"
+pem_file="yufei_aws2.pem"
 
+## echo colorful text
+red="\033[31m"
+normal="\033[m"
+
+echo -e "${red}hello red world${normal}"
 
 export AWS_DEFAULT_REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep region | awk -F\" '{print $4}'`
 instance_id=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
@@ -25,8 +27,22 @@ jump() {
     box_type=$1
     ip_list=$(aws ec2 describe-instances --filters "Name=tag:box_type, Values=$box_type" "Name=instance-state-name, Values=running" --query='Reservations[].Instances[].PrivateIpAddress' --output text)
   fi
+  
+  if [ -z "$ip_list" ]; then
+    echo -e "${red}No host found!${normal}"
+  fi
+  
+  echo -e "${red}add new feature: run \"dssh container\" for docker attach into container on host${normal}"
+  
+  for ip in $ip_list; do
+      ssh -o StrictHostKeyChecking=no -i ~/.ssh/${pem_file} $ip " type dssh  || echo 'dssh() { docker exec -it \$1 /bin/bash; }' >> .bashrc" > /dev/null
+  done
+  
+  for ip in $ip_list; do
+      echo -e "${red}jumping into $ip ${normal}"
+      screen -t $1-$ip ssh -t -o StrictHostKeyChecking=no -i ~/.ssh/${pem_file} $ip
+  done
+  
 }
 
-echo -e "${red}hello red world${normal}"
-echo -e "${purple}hello blue world${normal}"
-echo -e "hello world"
+
