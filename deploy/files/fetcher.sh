f64bcbf98d404f57de19adcfbb216f2b8782cb87
Yufei Ren <yufei.ren@thomsonreuters.com>
2017-06-14 14:06:20 +0800

    minor change

--- deploy/files/fetcher.sh
+++ deploy/files/fetcher.sh
@@ -6,14 +6,19 @@
 ## set exit when meet error
 set -e
 
+separate_line="******************************************"
+
 export AWS_DEFAULT_REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}'`
+echo $separate_line
 echo "AWS_DEFAULT_REGION = ${AWS_DEFAULT_REGION}"
 
 instance_id=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
+echo $separate_line
 echo "instance_id = ${instance_id}"
 
 box_type="`aws ec2 describe-tags --filters "Name=resource-id,Values=$instance_id" "Name=key,Values=box_type" --output=text --query Tags[].Value`"
 env="`echo $box_type |cut -f 1 -d "_"`"
 box_type_no_env="`echo $box_type |cut -f 2- -d "_"`"
+echo $separate_line
 echo "box_type = ${box_type}, env = ${env}, box_type_no_env = ${box_type_no_env}"
 
