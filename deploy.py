b36d8d85926457e9e55575667cd6cb7c04597349
Yufei Ren <yufei.ren@thomsonreuters.com>
2017-07-25 22:35:10 +0800

    minor change

--- deploy.py
+++ deploy.py
@@ -15,43 +15,46 @@ Example:
 '''
 
 
-def init_args(args):
+def init_args(input_vars):
     command_vars = {}
-    command_vars.update(vars(args))
-    
-    if not args.host_limit:
+    command_vars.update(vars(input_vars))
+
+    if not input_vars.host_limit:
         command_vars['host_limit'] = ''
-    
-    if not args.ip_limit:
+
+    if not input_vars.ip_limit:
         command_vars['ip_limit'] = ''
-    
-    if args.no_cache:
+
+    if input_vars.no_cache:
         command_vars['cache'] = 'disable'
     else:
         command_vars['cache'] = 'enable'
-    
+
     command_vars.pop('no_cache')
-    
-    if args.debug:
+
+    if input_vars.debug:
         command_vars['command'] = '/bin/bash'
         command_vars['tty'] = 'true'
     else:
         command_vars['command'] = ''
         command_vars['tty'] = 'false'
-        
+
     command_vars.pop('debug')
-    
+
     print command_vars
 
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(prog = 'deploy.py', 
-        description='this command for deploy docker image to aws')
-    parser.add_argument('env', help = 'environment: dev, qa, ppe, prod')
-    parser.add_argument('container', help = 'docker container: kiwi-ghost ...')
-    parser.add_argument('tag', help = 'docker image tag: 0.0.1, CKB-1000 ...')
-    parser.add_argument('-host', '--host_limit', help = 'limit host name to install, without env: -host blog')
-    parser.add_argument('-ip', '--ip_limit', help = 'limit ip to install: -ip 10.10.1.2')
-    parser.add_argument('-n', '--no_cache', action = 'store_true', help = 'disable apache page cache, works only for apache container')
-    parser.add_argument('-d', '--debug', action = 'store_true', help = 'enable debug model for container, start with container with /bin/bash')
+    parser = argparse.ArgumentParser(prog='deploy.py',
+                                     description='this command for deploy docker image to aws')
+    parser.add_argument('env', help='environment: dev, qa, ppe, prod')
+    parser.add_argument('container', help='docker container: kiwi-ghost ...')
+    parser.add_argument('tag', help='docker image tag: 0.0.1, CKB-1000 ...')
+    parser.add_argument('-host', '--host_limit', help='limit host name to install, without env: -host blog')
+    parser.add_argument('-ip', '--ip_limit', help='limit ip to install: -ip 10.10.1.2')
+    parser.add_argument('-n', '--no_cache', action='store_true',
+                        help='disable apache page cache, works only for apache container')
+    parser.add_argument('-d', '--debug', action='store_true',
+                        help='enable debug model for container, start with container with /bin/bash')
     args = parser.parse_args()
-    command_vars = init_args(args)
+    init_args(args)
